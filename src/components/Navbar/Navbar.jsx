177f4fa9046df6a26cb0016282a01861a0eaca63
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2023-02-11 20:27:00 +0300

    Building the Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-
+import images from '../../constants/images'
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className='app__navbar'>
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="app logo" />
+    </div>
+    <ul className='app__navbar-links'>
+      <li className="p__opensans"><a href="#home">Home</a></li>
+      <li className="p__opensans"><a href="#about">About</a></li>
+      <li className="p__opensans"><a href="#menu">Menu</a></li>
+      <li className="p__opensans"><a href="#awards">Awards</a></li>
+      <li className="p__opensans"><a href="#home">Home</a></li>
+    </ul>
+    <div className="app__navbar-login">
+      <a href="#login" className='p__opensans'>Log in / Register</a>
+      <div/>
+      <a href="/" className="p__opensans">Book Table</a>
+    </div>
+  </nav>
 );
 
 export default Navbar;
